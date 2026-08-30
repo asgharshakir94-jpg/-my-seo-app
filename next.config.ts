47ecf7a2505b395c34e71329f72252c15b03d6be
Muhammd Asghar Shakir <asgharshakir94@gmail.com>
2026-08-30 19:48:46 +0500

    chore: enforce trailing slash sitewide

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,15 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
+  // ✅ Force trailing slashes sitewide
+  trailingSlash: true,
+
   typescript: {
     // Dangerously allow production builds to successfully complete even if
     // your project has TypeScript errors.
     ignoreBuildErrors: true,
   },
+
   async redirects() {
     return [
       {
